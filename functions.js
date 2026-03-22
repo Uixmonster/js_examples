11f1f881f254020f466cfd4eddbdc5adae402f2f
Uixmonster <andriimaksim@gamil.com>
2026-03-22 18:26:18 +0100

    add ex

--- functions.js
+++ functions.js
@@ -40,3 +40,82 @@ console.log(pow(1, 100));
 // } else {
 //   alert(`Степень ${n} не поддерживается, используйте натуральное число`);
 // }
+
+
+// callback functions
+
+function ask(question, showOk, showCancel) {
+  if (confirm(question)) showOk()
+  else showCancel();
+}
+
+function showOk() {
+  alert( "Вы согласны." );
+}
+
+function showCancel() {
+  alert( "Вы отменили выполнение." );
+}
+
+// использование: функции showOk, showCancel передаются в качестве аргументов ask
+ask("Вы согласны?", showOk, showCancel);
+
+
+
+function ask(question, yes, no) {
+  if (confirm(question)) yes()
+  else no();
+}
+
+ask(
+  "Вы согласны?",
+  function() { alert("Вы согласились."); },
+  function() { alert("Вы отменили выполнение."); }
+);
+
+// Здесь функции объявляются прямо внутри вызова ask(...). У них нет имён, поэтому они называются анонимными. Такие функции недоступны снаружи ask (потому что они не присвоены переменным), но это как раз то, что нам нужно.
+
+
+// Function Declaration
+function sum(a, b) {
+  return a + b;
+}
+
+
+// Function Expression
+let sum = function(a, b) {
+  return a + b;
+};
+
+
+// Более тонкое отличие состоит в том, когда создаётся функция движком JavaScript.
+
+// Function Expression создаётся, когда выполнение доходит до него, и затем уже может использоваться.
+
+// После того, как поток выполнения достигнет правой части выражения присваивания let sum = function… – с этого момента, функция считается созданной и может быть использована (присвоена переменной, вызвана и т.д. ).
+
+// С Function Declaration всё иначе.
+
+// Function Declaration может быть вызвана раньше, чем она объявлена.
+
+// Другими словами, когда движок JavaScript готовится выполнять скрипт или блок кода, прежде всего он ищет в нём Function Declaration и создаёт все такие функции. Можно считать этот процесс «стадией инициализации».
+
+let age = prompt("Сколько Вам лет?", 18);
+
+let welcome;
+
+if (age < 18) {
+
+  welcome = function() {
+    alert("Привет!");
+  };
+
+} else {
+
+  welcome = function() {
+    alert("Здравствуйте!");
+  };
+
+}
+
+welcome();  
